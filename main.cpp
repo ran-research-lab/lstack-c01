26ff1e10626156ea6bb448392a07ec0695d64011
Rafael Rafael <rafael.arce@upr.edu>
2021-03-23 00:21:00 +0000

    Removed catch.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,42 @@
 // Su nombre aqui
 
-#define CATCH_CONFIG_MAIN  
-#include "catch.hpp"
+
 #include "Stack.h"
 #include "LStack.h"
 using namespace std;
 
+// Variables and macros for assertions, DO NOT CHANGE ****
+int totalAssertions = 0;
+int passedAssertions  = 0;
+#define expect( chk )  \
+    totalAssertions++; \
+    if (!(chk)) \
+        printf("\x1B[31mAssertion (%s) failed %s at line %d\033[0m\t\t\n", #chk, __FILE__,__LINE__); \
+    else { \
+        printf("\x1B[32mPassed line %d: %s\033[0m\t\t\n", __LINE__, #chk); \
+        passedAssertions++; \
+    }
+#define assertionReport() { \
+     printf("==========================================\n"); \
+     printf("Passed %d of %d assertions\n",passedAssertions,totalAssertions); \
+     printf("\n"); \
+    }
+//*******************************************************
 
-TEST_CASE( "Pushing and popping to the AStack", "[AStack]" ) {
+
+int main() {
 	LStack S(20);
 	for(int i = 0; i < 10; i++) S.push(i*10);
-    CHECK( S.toString() == "90 80 70 60 50 40 30 20 10 0 ");
+    expect( S.toString() == "90 80 70 60 50 40 30 20 10 0 ");
     int ctr = 0;
+
+    S.pop(); S.pop();
+    expect( S.toString() == "70 60 50 40 30 20 10 0 ");
+
     while(!S.isEmpty()) {
         S.pop();
         ctr++;
     }
-    CHECK( ctr == 10);
+    expect(ctr == 8);
+    assertionReport();
 }
